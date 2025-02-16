ad97a46ce9025fca513ed0833b2c80c1cd83f342
vangitty <77495065+vangitty@users.noreply.github.com>
2025-02-16 19:04:12 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,5 +7,10 @@ chown -R nobody:nogroup /var/run/tinyproxy /var/log/tinyproxy
 # Tinyproxy im Hintergrund starten
 tinyproxy -c /etc/tinyproxy/tinyproxy.conf
 
-# OpenVPN starten (im Vordergrund)
-exec openvpn --config /vpn/config.ovpn
+# Auth-Datei aus Umgebungsvariablen erstellen
+echo "$OVPN_USERNAME" > /vpn/auth.txt
+echo "$OVPN_PASSWORD" >> /vpn/auth.txt
+chmod 600 /vpn/auth.txt
+
+# OpenVPN mit auth-file starten
+exec openvpn --config /vpn/config.ovpn --auth-user-pass /vpn/auth.txt --auth-nocache
